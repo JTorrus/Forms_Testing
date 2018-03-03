b7070c9c3fee5bfd1de7987521897ad227f83e78
JTorrus <jtorrusgalindo@gmail.com>
2018-03-03 03:54:31 +0100

    Comparator methods and testing implemented

--- src/test_pkg/Comparator.java
+++ src/test_pkg/Comparator.java
@@ -1,31 +1,23 @@
 package test_pkg;
 
 class Comparator {
-    private Figure figureOne;
-    private Figure figureTwo;
-
-    public Comparator(Figure figureOne, Figure figureTwo) {
-        this.figureOne = figureOne;
-        this.figureTwo = figureTwo;
-    }
-
-    void compareArea(Figure figureOne, Figure figureTwo) {
+    int compareArea(Figure figureOne, Figure figureTwo) {
         if (figureOne.getArea() > figureTwo.getArea()) {
-
+            return 1;
         } else if (figureOne.getArea() < figureTwo.getArea()) {
-
+            return -1;
         } else {
-
+            return 0;
         }
     }
 
-    void comparePerimeter(Figure figureOne, Figure figureTwo) {
+    int comparePerimeter(Figure figureOne, Figure figureTwo) {
         if (figureOne.getPerimeter() > figureTwo.getPerimeter()) {
-
+            return 1;
         } else if (figureOne.getPerimeter() < figureTwo.getPerimeter()) {
-
+            return -1;
         } else {
-
+            return 0;
         }
     }
 }
--- test/test_pkg/ComparatorTest.java
+++ test/test_pkg/ComparatorTest.java
@@ -1,4 +1,56 @@
 package test_pkg;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ComparatorTest {
+    private Comparator comparator = new Comparator();
+
+    @Test
+    public void ifFirstFigureAreaIsGreaterThanSecondOneItShouldReturnIntegerOne() {
+        Circle circle = new Circle(10);
+        Rectangle rectangle = new Rectangle(10, 20);
+
+        Assert.assertEquals(1, comparator.compareArea(circle, rectangle));
+    }
+
+    @Test
+    public void ifFirstFigureAreaIsLowerThanSecondOneItShouldReturnIntegerOneNegative() {
+        Rectangle rectangle = new Rectangle(10, 20);
+        Square square = new Square(40);
+
+        Assert.assertEquals(-1, comparator.compareArea(rectangle, square));
+    }
+
+    @Test
+    public void ifFirstFigurePerimeterIsGreaterThanSecondOneItShouldReturnIntegerOne() {
+        Circle circle = new Circle(10);
+        Rectangle rectangle = new Rectangle(10, 20);
+
+        Assert.assertEquals(1, comparator.comparePerimeter(circle, rectangle));
+    }
+
+    @Test
+    public void ifFirstFigurePerimeterIsLowerThanSecondOneItShouldReturnIntegerOneNegative() {
+        Rectangle rectangle = new Rectangle(10, 20);
+        Square square = new Square(40);
+
+        Assert.assertEquals(-1, comparator.comparePerimeter(rectangle, square));
+    }
+
+    @Test
+    public void ifFirstFigureAreaIsEqualToSecondOneItShouldReturnIntegerZero() {
+        Rectangle rectangle = new Rectangle(800, 2);
+        Square square = new Square(40);
+
+        Assert.assertEquals(0, comparator.compareArea(rectangle, square));
+    }
+
+    @Test
+    public void ifFirstFigurePerimeterIsEqualToSecondOneItShouldReturnIntegerZero() {
+        Rectangle rectangle = new Rectangle(40, 40);
+        Square square = new Square(40);
+
+        Assert.assertEquals(0, comparator.comparePerimeter(rectangle, square));
+    }
 }
